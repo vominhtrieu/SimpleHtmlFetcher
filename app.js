896a1dbfafbae14276f82590a4655339a3a86f0d
vominhtrieu <minhtrieuvo600@gmail.com>
2022-03-09 21:21:46 +0700

    Basic implement

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const puppeteer = require("puppeteer");
+const cors = require("cors");
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
+app.use(cors());
+app.use((_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    next();
+});
+
+app.post("/", async (req, res) => {
+    try {
+        console.log(req.body)
+        const browser = await puppeteer.launch();
+        const [page] = await browser.pages();
+        console.log(req.body.url);
+        await page.goto(req.body.url, { waitUntil: 'domcontentloaded' });
+        const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+
+        await browser.close();
+        res.send(data);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(400);
+    }
+})
+app.listen(3000, () => console.log("Server has been started"));
